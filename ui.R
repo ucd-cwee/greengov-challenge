e6057f78db0c67dd37bedca77a3140e8d57abf75
Andrew Holguin <ajholguin@ucdavis.edu>
2016-04-15 19:05:56 -0700

    use html template for ui

--- ui.R
+++ ui.R
@@ -1,23 +1,5 @@
 
-fluidPage(
-  
-  # Application title
-  titlePanel("Test"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      waterConservationInput1('water_districts'),
-      waterConservationInput2('water_districts')
-    ),
-    
-    mainPanel(
-      waterConservationOutput1('water_districts'),
-      waterConservationOutput2('water_districts'),
-      textOutput('savings'),
-      textOutput('vs_std'),
-      textOutput('ei'),
-      textOutput('ghg')
-    )
-  )
-)
+# use an html template for ui
+htmlTemplate("template.html")
+
+
